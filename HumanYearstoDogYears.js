be0d5a83c3b7e952926cb653bce78764b60d43ed
Jennifer Arce-Navarrete <99854742+jarcenav@users.noreply.github.com>
2023-02-03 08:18:25 -0500

    Add files via upload

---
+++ HumanYearstoDogYears.js
@@ -0,0 +1,18 @@
+//Stating my age 
+var myAge = 23;
+//Setting a value for variable as a placeholder
+let earlyYears = 2;
+
+//this might break copy code from hint
+earlyYears = earlyYears * 10.5;
+let laterYears = myAge - 2;
+//Calculate number of dog years
+laterYears *= 4;
+//The following console.log(s) are to check of the values of the variable are correct
+console.log(earlyYears);
+console.log(laterYears);
+//Calculation to determine my age in dog years
+var myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Jenny';
+console.log(myName.toLowerCase());
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)

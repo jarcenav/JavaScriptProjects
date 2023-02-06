27b66d60431aec0436301d8724e322cd7b467208
Jennifer Arce-Navarrete <99854742+jarcenav@users.noreply.github.com>
2023-02-06 13:19:30 -0500

    Add files via upload

---
+++ RaceDay.js
@@ -0,0 +1,48 @@
+/*
+Codecademy’s annual race is just around the corner! 
+This year, we have a lot of participants. 
+You have been hired to write a program that will register runners for the race and give them instructions on race day.
+Here’s how our registration works. There are adult runners (over 18 years of age) and youth runners (under 18 years of age). 
+They can register early or late. Runners are assigned a race number and start time based on their age and registration.
+Early adults receive a race number at or above 1000.
+All others receive a number below 1000.
+Adult registrants run at 9:30 am or 11:00 am.
+Early adults run at 9:30 am.
+Late adults run at 11:00 am.
+Youth registrants run at 12:30 pm (regardless of registration).
+*/
+let raceNumber = Math.floor(Math.random() * 1000);
+const earlyRegistration = true;
+var runnersAge = 16;
+if (runnersAge >= 18 && earlyRegistration)
+{
+raceNumber += 1000;
+}
+else if (runnersAge >= 18 && !earlyRegistration)
+{
+  raceNumber < 1000;
+}
+if (runnersAge > 18 && earlyRegistration)
+{
+  console.log(`Since you are ${runnersAge} and have registered early you will race at 9:30 am.`)
+  console.log(`Your race number is ${raceNumber}.`)
+}
+else if (runnersAge > 18 && !earlyRegistration)
+{
+  console.log(`Since you are ${runnersAge} and have not registered early you will race at 11:00 am.`);
+  console.log(`Your race number is ${raceNumber}.`);
+}
+else if (runnersAge < 18)
+{
+  console.log(`Since you are ${runnersAge} you will race at 12:30 pm.`);
+  console.log(`Your race number is ${raceNumber}.`);
+}
+if (runnersAge === 18 && earlyRegistration)
+{
+  console.log('Please visit the registration desk!');
+}
+else if (runnersAge === 18 && !earlyRegistration)
+{
+  console.log('Please visit the registration desk.');
+}
+
